5bf2c41452a1db14e6656638ee314dc65324e410
aigib <aigibuht@gmail.com>
2024-10-27 22:22:15 +0200

    changed font family, search city, font sizes and color

--- src/index.js
+++ src/index.js
@@ -53,4 +53,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Paris");
+searchCity("London");
